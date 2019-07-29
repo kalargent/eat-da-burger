a5376825bbe01cf7301ff257bbfc0990d3f03208
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-07-29 16:41:54 -0400

    updated to take in the JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,26 @@ var PORT = process.env.PORT || 3000;
 
 // DATABASE CONNECTION
 // connection var
-var connection = mysql.createConnection ({
-    host: "localhost", 
-    port: 3306, 
-    user: "root", 
-    password: "", 
-    database: "burger_db" 
-}); 
+var connection ; 
+
+if (process.env.JAWSDB_URL) { 
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else { 
+    connection = mysql.createConnection({
+        host: "localhost", 
+        port: 3306, 
+        user: "root", 
+        password: "", 
+        database: "burger_db"
+    })
+}
+// var connection = mysql.createConnection ({
+//     host: "localhost", 
+//     port: 3306, 
+//     user: "root", 
+//     password: "", 
+//     database: "burger_db" 
+// }); 
 
 // actually connecting to the db 
 connection.connect (function (err) {
